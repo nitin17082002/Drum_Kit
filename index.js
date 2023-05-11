ca7460f4049bf5c409cb7c788f90451024f972e4
nitin17082002 <nitin17082002@gmail.com>
2023-05-11 15:35:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i=0;i<noOfDrumButtons;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        console.log(this);
+    });
+}
